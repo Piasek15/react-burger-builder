f1bc4af07b5fb771da735de33122a4635a814465
patrick <patrick.piasecki@nask.pl>
2020-01-03 17:25:07 +0100

    adding BurgerBuilder component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Aux from '../../hoc/Aux'
-import classes from './Layout.css'
+import classes from './Layout.module.css'
 
 const layout = ( props ) => (
     <Aux>
