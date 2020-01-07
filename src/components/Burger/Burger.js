a2128f7b1ce552ea97156e56e3865db9ae9a90a7
patrick <patrick.piasecki@nask.pl>
2020-01-07 15:14:56 +0100

    simplify ingredients array.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,20 @@ import classes from './Burger.module.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
-    const ingredients = Object.keys(props.ingredients)
+    let ingredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
                 return <BurgerIngredient key={igKey + i} type={igKey}/>;
             })
-        });
+        }).reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+
+    console.log(ingredients);
+
+    if (ingredients.length === 0){
+        ingredients = <p>Please start adding ingredients</p>
+    }
 
     return (
         <div className={classes.Burger}>
