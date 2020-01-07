837b115a9ca3a2aa389765490a523fe59434cfde
patrick <patrick.piasecki@nask.pl>
2020-01-07 17:02:43 +0100

    adding removing ingredients functionality and price counter.

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -5,8 +5,15 @@ import classes from './BuildControl.module.css'
 const buildControl = (props) => (
     <div className={classes.BuildControl}>
         <div className={classes.Label}>{props.label}</div>
-        <button className={classes.Less}>-</button>
-        <button className={classes.More} onClick={props.added}>+</button>
+        <button
+            className={classes.Less}
+            onClick={props.removed}
+            disabled={props.disabled}
+        >-</button>
+        <button
+            className={classes.More}
+            onClick={props.added}
+        >+</button>
     </div>
 );
 
--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -12,11 +12,14 @@ const constrols = [
 
 const buildControls = (props) => (
     <div className={classes.BuildControls}>
+        <p>Current Price: <strong>{props.price.toFixed(2)}</strong> zł</p>
         {constrols.map(ctrl => (
             <BuildControl
                 key={ctrl.label}
                 label={ctrl.label}
                 added={() => props.ingredientAdded(ctrl.type)}
+                removed={() => props.ingredientRemoved(ctrl.type)}
+                disabled={props.disabled[ctrl.type]}
                 />
         ))}
     </div>
