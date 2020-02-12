15122d4c6d5c6d550fa08ec0ebfb29534f9052bc
Patrick Piasecki <piasecki.patrick.15@gmail.com>
2020-02-12 15:48:15 +0100

    adding redux-thunk

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import {createStore, applyMiddleware, compose} from 'redux';
 import reducer from "./store/reducer";
 import {Provider} from "react-redux";
+import thunk from 'redux-thunk';
 
 const logger = store => {
     return next => {
@@ -20,7 +21,7 @@ const logger = store => {
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducer, composeEnhancers(applyMiddleware(logger)));
+const store = createStore(reducer, composeEnhancers(applyMiddleware(logger, thunk)));
 
 ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementById('root'));
 
--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -1,11 +1,19 @@
 export const ADD_INGREDIENT = 'ADD_INGREDIENT';
 export const REMOVE_INGREDIENT = 'REMOVE_INGREDIENT';
 
-export const addIngredient = (ingredient) => {
+export const saveIngredient = (ingredient) => {
     return {
         type: ADD_INGREDIENT,
         ingredient: ingredient
     }
+};
+
+export const addIngredient = (ingredient) => {
+    return dispatch => {
+        setTimeout(() => {
+            dispatch(saveIngredient(ingredient))
+        }, 2000);
+    };
 };
 
 export const removeIngredient = (ingredient) => {
