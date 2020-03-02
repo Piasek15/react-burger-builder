8c026ffac8a215eda7e7185fee44d478ba3db58e
Patrick Piasecki <piasecki.patrick.15@gmail.com>
2020-03-02 14:45:13 +0100

    renaming Aux to Auxiliary

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import Aux from "../../../hoc/Aux/Aux";
+import Aux from "../../../hoc/Auxiliary/Auxiliary";
 import Button from "../../UI/Button/Button";
 
 const orderSummary = (props) => {
