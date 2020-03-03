1cf1bfe748a02ab60a17d4d2adc4a9ea4595e349
Patrick Piasecki <piasecki.patrick.15@gmail.com>
2020-03-03 17:06:21 +0100

    orders in redux

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -33,6 +33,19 @@ const reducer = (state = initialState, action) => {
             return updateObject(state, {
                 loading: false
             });
+        case actionTypes.FETCH_ORDERS_START:
+            return updateObject(state, {
+                loading: true
+            });
+        case actionTypes.FETCH_ORDERS_SUCCESS:
+            return updateObject(state, {
+                loading: false,
+                orders: action.orders
+            });
+        case actionTypes.FETCH_ORDERS_FAIL:
+            return updateObject(state, {
+                loading: false
+            });
         default:
             return state;
 
